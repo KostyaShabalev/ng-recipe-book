bc4ccf331455c112779c8c983900988f4c01004f
kshab <kvshabalev@gmail.com>
2020-01-20 21:17:54 +0200

    add shopping-list service

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ShoppingListService } from './services/shopping-list.service';
+import { Ingredient } from './../shared/models/ingredient.model';
 
 @Component({
     selector: 'app-shopping-list',
     templateUrl: './shopping-list.component.html',
     styleUrls: ['./shopping-list.component.scss']
 })
-export class ShoppingListComponent {}
+export class ShoppingListComponent implements OnInit {
+    public ingredients: Ingredient[];
+
+    constructor(
+        private shoppingListService: ShoppingListService
+    ) {}
+
+    ngOnInit() {
+        this.shoppingListService.getIngredients();
+    }
+}
