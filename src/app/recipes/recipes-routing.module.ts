a155014ecaa638fb91bdbe4edb242d9378c8b921
kshab <kvshabalev@gmail.com>
2020-02-09 10:49:18 +0200

    add authGuard

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -3,13 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { RecipesComponent } from './recipes.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
-import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
-
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';import { AuthGuard } from './../auth/auth.guard';
 
 const routes: Routes = [
     {
         path: 'recipes',
         component: RecipesComponent,
+        canActivate: [AuthGuard],
         children: [
             { path: 'new', component: RecipeEditComponent },
             { path: ':id', component: RecipeDetailComponent },
